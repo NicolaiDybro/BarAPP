aec5d482fdf9da5c4bcb368820359c7caab15383
Nicolai <nicolai160g@gmail.com>
2023-03-10 12:56:09 +0100

    Initial commit

---
+++ app/src/main/java/user/nicolai/barapp/FirstFragment.java
@@ -0,0 +1,196 @@
+package user.nicolai.barapp;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.text.BreakIterator;
+import java.util.Set;
+import java.util.UUID;
+
+import user.nicolai.barapp.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+
+    public BluetoothAdapter bluetoothAdapter;
+    private FragmentFirstBinding binding;
+    ThreadConnectBTdevice myThreadConnectBTdevice;
+    ThreadConnected myThreadConnected;
+    /* access modifiers changed from: private */
+    private UUID myUUID;
+    /* access modifiers changed from: private */
+    public Set<BluetoothDevice> pairedDevices;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+      binding = FragmentFirstBinding.inflate(inflater, container, false);
+      return binding.getRoot();
+
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            public void onClick(View view) {
+                myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+                bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableAdapter, 0);
+                pairedDevices = bluetoothAdapter.getBondedDevices();
+                for (BluetoothDevice device : pairedDevices) {
+                    System.out.println(device.getName() + ": " + device.getAddress());
+                    if (device.getName().contains("raspberrypi")) {
+                        if (myThreadConnected == null) {
+                            myThreadConnectBTdevice = new ThreadConnectBTdevice(device);
+                            myThreadConnectBTdevice.start();
+                            NavHostFragment.findNavController(FirstFragment.this)
+                                    .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                        } else {
+                            myThreadConnected.write("test".getBytes());
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+
+    public void startThreadConnected(BluetoothSocket socket) {
+        ThreadConnected threadConnected = new ThreadConnected(socket);
+        myThreadConnected = threadConnected;
+        threadConnected.start();
+    }
+
+    private static class ThreadConnected extends Thread {
+        private final BluetoothSocket connectedBluetoothSocket;
+        private final InputStream connectedInputStream;
+        private final OutputStream connectedOutputStream;
+
+        public ThreadConnected(BluetoothSocket socket) {
+            this.connectedBluetoothSocket = socket;
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = socket.getInputStream();
+                out = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            this.connectedInputStream = in;
+            this.connectedOutputStream = out;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+            while (true) try {
+                try {
+                    bytes = connectedInputStream.read(buffer);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                String strReceived = new String(buffer, 0, bytes);
+                final String msgReceived = String.valueOf(bytes) +
+                        " bytes received:\n"
+                        + strReceived;
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void write(byte[] buffer) {
+            try {
+                this.connectedOutputStream.write(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void cancel() {
+            try {
+                this.connectedBluetoothSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void onDestroyView() {
+        super.onDestroyView();
+        this.binding = null;
+    }
+
+    public class ThreadConnectBTdevice extends Thread{
+        private BluetoothSocket bluetoothSocket = null;
+        private final BluetoothDevice bluetoothDevice;
+
+
+        @SuppressLint("MissingPermission")
+        ThreadConnectBTdevice(BluetoothDevice device) {
+            bluetoothDevice = device;
+            try {
+                bluetoothSocket = (BluetoothSocket) this.bluetoothDevice.getClass().getMethod("createRfcommSocket", Integer.TYPE).invoke(this.bluetoothDevice, new Object[]{1});
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @SuppressLint("MissingPermission")
+        public void run() {
+            boolean success = false;
+            try {
+                bluetoothSocket.connect();
+                success = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+                final String eMessage = e.getMessage();
+                try {
+                    bluetoothSocket.close();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+            if (success) {
+                startThreadConnected(bluetoothSocket);
+                System.out.println("Connected to: " + this.bluetoothDevice.getName());
+                return;
+            }
+            System.out.println("FEJL!!");
+        }
+
+        public void cancel() {
+            try {
+                this.bluetoothSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/user/nicolai/barapp/MainActivity.java
@@ -0,0 +1,44 @@
+package user.nicolai.barapp;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import user.nicolai.barapp.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding inflate = ActivityMainBinding.inflate(getLayoutInflater());
+        this.binding = inflate;
+        setContentView((View) inflate.getRoot());
+        this.appBarConfiguration = new AppBarConfiguration.Builder(Navigation.findNavController(this, R.id.nav_host_fragment_content_main).getGraph()).build();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment_content_main), this.appBarConfiguration) || super.onSupportNavigateUp();
+    }
+}
